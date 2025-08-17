d32e334178dd8eede8650004161198e785accb8a
Nikunj Patel <nikunjpatel1905@gmail.com>
2025-08-17 03:42:40 -0400

    Set up Hilt dependency injection with AppModule and initialized TicTacToeApp

--- app/src/main/java/com/nikunjprojects/onlinetictactoe/TicTacToeApp.kt
+++ app/src/main/java/com/nikunjprojects/onlinetictactoe/TicTacToeApp.kt
@@ -1,4 +1,7 @@
 package com.nikunjprojects.onlinetictactoe
 
-class TicTacToeApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TicTacToeApp : Application()
--- app/src/main/java/com/nikunjprojects/onlinetictactoe/di/AppModule.kt
+++ app/src/main/java/com/nikunjprojects/onlinetictactoe/di/AppModule.kt
@@ -1,4 +1,33 @@
 package com.nikunjprojects.onlinetictactoe.di
 
+import com.nikunjprojects.onlinetictactoe.data.KtorRealTimeMessagingClient
+import com.nikunjprojects.onlinetictactoe.data.RealTimeMessagingClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.features.websocket.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(CIO) {
+            install(Logging)
+            install(WebSockets)
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideRealtimeMessagingClient(httpClient: HttpClient): RealTimeMessagingClient {
+        return KtorRealTimeMessagingClient(httpClient)
+    }
 }
